7c5be50e0da8963e1a526501e629b68bb3794cde
diego <jdiegogarciaco@gmail.com>
2022-10-22 19:04:29 +0200

    01 - 02, done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+function repeatString(text, number) {
+    if (number > 0) {
+        return text.repeat(number);
+    } else if (number < 0) {
+        return "ERROR";
+    } return "";
 };
+
+const number = Math.floor(Math.random() * 1000);
+
+repeatString('hey', number).match(/((hey))/g).length;
+
 
 // Do not edit below this line
 module.exports = repeatString;
